aa9845c98e21146b87471f294709819e671565a8
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-04-28 19:00:10 +0300

    fix: reomve missing all_countries dataset and fix streamlit loading

--- app/main.py
+++ app/main.py
@@ -3,26 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load data (you must combine your CSVs into one file OR load multiple)
+# LOAD DATASETS
 ethiopia = pd.read_csv("data/ethiopia_clean.csv")
 kenya = pd.read_csv("data/kenya_clean.csv")
 sudan = pd.read_csv("data/sudan_clean.csv")
-tanzania = pd.read_csv("data/tanzania_clean.csv")    
-nigeria = pd.read_csv("data/nigeria_clean.csv")  
-
+tanzania = pd.read_csv("data/tanzania_clean.csv")
+nigeria = pd.read_csv("data/nigeria_clean.csv")
 
 df = pd.concat([ethiopia, kenya, sudan, tanzania, nigeria])
 
-st.title("Climate Dashboard")
+st.title(" Climate Dashboard")
 
-#  COUNTRY SELECTOR
+# COUNTRY SELECTOR
 countries = st.multiselect(
     "Select Country",
     df["Country"].unique(),
-    default=df["Country"].unique()
+    default=list(df["Country"].unique())
 )
 
-# YEAR FILTER 
+# YEAR FILTER
 year_range = st.slider(
     "Select Year Range",
     int(df["YEAR"].min()),
@@ -30,26 +29,20 @@ year_range = st.slider(
     (2015, 2026)
 )
 
-# VARIABLE SELECTOR 
-variable = st.selectbox(
-    "Select Variable",
-    ["T2M", "PRECTOTCORR", "RH2M"]
-)
-
-# FILTER DATA 
+# FILTER DATA
 filtered = df[
     (df["Country"].isin(countries)) &
     (df["YEAR"] >= year_range[0]) &
     (df["YEAR"] <= year_range[1])
 ]
 
-# TEMPERATURE TREND 
+# TEMPERATURE TREND
 st.subheader("Temperature Trend")
 
 temp = filtered.groupby(["YEAR", "Country"])["T2M"].mean().unstack()
 st.line_chart(temp)
 
-# PRECIPITATION BOXPLOT 
+# PRECIPITATION BOXPLOT
 st.subheader("Precipitation Distribution")
 
 fig, ax = plt.subplots()
